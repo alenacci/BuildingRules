7b4ea7babd0242e5de47640f9fbec1591d1930eb
buildingdepot <buildingdepot@ubuntu.(none)>
2014-03-13 10:44:34 -0700

    test dbg

--- backend/app/backend/controller/actionExecutor.py
+++ backend/app/backend/controller/actionExecutor.py
@@ -278,6 +278,9 @@ class ActionExecutor:
 					alreadyAppliedCategories = []
 					for rule in ruleList:
 
+						print "=============================="
+						print ruleList
+						print "=============================="
 						if rule.getPriority() == 0:
 							raw_input("VEDIAMO UN PO CHE SUCCEDE .... " + str(rule))
 						
