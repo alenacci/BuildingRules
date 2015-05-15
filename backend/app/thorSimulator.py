7fbb6f3cc61895acc3ae40a834099dbfe1204341
Jaco-F <jacopo.f91@gmail.com>
2015-05-15 14:19:25 +0200

    thorSimulator v2

--- backend/app/thorSimulator.py
+++ backend/app/thorSimulator.py
@@ -1,4 +1,5 @@
 import os
+import math
 from flask import json
 
 from app.backend.controller.buildingsManager import BuildingsManager
@@ -29,8 +30,12 @@ def start():
     buildings = buildingsManager.getAllBuildings()
 
     internalTempDict = {}
-    windowsStatus = ""
-    hvacTemp = 0
+
+
+
+
+
+
 
     for building in buildings["buildings"]:
         rooms = buildingsManager.getRooms(building["buildingName"],username)
@@ -41,14 +46,27 @@ def start():
                 if not os.path.exists("tools/simulation/ThorSim/"+building["buildingName"]+"/"+room["roomName"]): os.makedirs("tools/simulation/ThorSim/"+building["buildingName"]+"/"+room["roomName"])
 
                 #INIT
-                intTemp = 0 #init temp
+                intTemp = 55 #init temp
                 windowsStatus = "CLOSE"
+                hvacStatus = False
+                hvacTemp = 0
+
+                transientExtTemp = 0.0
+                deltaTWindow = 0.0
+                transientDurationWindow = 0
+                transientExpCoefWindow=0.0
+
+
+                transientHvacTemp = 0.0
+                deltaTHvac = 0.0
+                transientDurationHvac = 0
+                transientExpCoefHvac=0.0
+
                 for simulation in roomData.items():
                     for hour in range(0,24,1):
                         extTemp = getExtTemp(building["buildingName"], hour)
 
                         for category in simulation[1]["simulation"].items() :
-                            print category
                             for ruleDetail in category[1]:
                                 if float(ruleDetail["from"].replace(":",".")) <= hour <= float(ruleDetail["to"].replace(":",".")) :
 
@@ -60,30 +78,52 @@ def start():
                                         newTemps.append(temperatures[0].replace("F",""))
                                         newTemps.append(temperatures[1].replace("F",""))
                                         #facciamo la media delle 2 temp nel set?
-                                        hvacTemp = newTemps[0]
+                                        hvacTemp = int(newTemps[0])
+                                        hvacStatus = True
 
 
 
                         if windowsStatus == "OPEN":
-
-                            print("implement windows opened")
-
                             if extTemp != intTemp:
-                                print("implement inttemp != exttemp")
+                                if transientExtTemp != extTemp:
+                                    transientExtTemp = extTemp
+                                    deltaTWindow = (extTemp - intTemp)
+                                    transientExpCoefWindow = float(abs(deltaTWindow))/float(max(extTemp,intTemp))
+                                    transientDurationWindow = 1
+                                else:
+                                    transientDurationWindow +=1
+                                intTemp = extTemp - (deltaTWindow)*math.exp(-transientExpCoefWindow*transientDurationWindow)
+
+                                transientHvacTemp = 0.0
+
                                 #recalculate each hour based on the updated extTemp, behave like RC with tau = 5 to reach extTemp
-
-                        else:
-                            print("implement windows closed")
-                            if intTemp < hvacTemp:
-                                print("implement inttemp < hvactemp")
-                                #behave like RC with tau = 5
-                            if intTemp > hvacTemp:
-                                print("implement inttemp > hvactemp")
-                                #behave like RC discharging tau = 5
+                        elif hvacStatus:
+                            if intTemp != hvacTemp:
+                                if transientHvacTemp != hvacTemp:
+                                    transientHvacTemp = hvacTemp
+                                    deltaTHvac = (hvacTemp - intTemp)
+                                    transientExpCoefHvac = float(abs(deltaTHvac))/float(max(hvacTemp,intTemp))
+                                    transientDurationHvac = 1
 
 
-                        internalTempDict[hour] = intTemp
-                #saveJSON internalTemp
+                                else:
+                                    transientDurationHvac +=1
+
+                                intTemp = hvacTemp - (deltaTHvac)*math.exp(-transientExpCoefHvac*transientDurationHvac)
+
+                                transientExtTemp = 0.0
+
+                        saveData = {}
+                        saveData["windows"] = windowsStatus
+                        saveData["hvac"] = hvacTemp
+                        saveData["intTemp"] = intTemp
+                        saveData["extTemp"] = extTemp
+                        internalTempDict[hour] = saveData
+
+                jsonResultsPath = "tools/simulation/ThorSim/"+building["buildingName"]+"/"+room["roomName"]+ "/ThorSimResultsData.json"
+                outputResults = open(jsonResultsPath,'wb')
+                json.dump(internalTempDict,outputResults)
+
 
 
 
@@ -98,4 +138,4 @@ def fetchDataFromJSON(roomName):
 
 
 def getExtTemp(buildingName, hour):
-    return 15
+    return 40
