76c09f71c20daf384d5a2f639e706a65ce832d22
andreuke <andreatello92@gmail.com>
2016-02-05 17:27:09 +0100

    conflict resolution using Satisfaction and Majority Weighted Vote

--- backend/app/backend/controller/actionExecutor.py
+++ backend/app/backend/controller/actionExecutor.py
@@ -28,6 +28,8 @@ from app.backend.model.buildings import Buildings
 from app.backend.model.rules import Rules
 from app.backend.model.rule import Rule
 
+from app.backend.controller.satisfactionManager import SatisfactionManager
+
 
 class ActionExecutor:
     def __init__(self, simulationParameters=None, roomFilter=None,buildingFilter=None):
@@ -558,147 +560,88 @@ class ActionExecutor:
             flash(cat, "blue")
             print rules
 
-            if len(rules) > 1 and cat in ['HVAC_HUM', 'HVAC_TEMP']:
-                return self.conflictResolution(rules)
+            if len(rules) > 1:
+                return self.conflictResolution(rules, cat)
             else:
-                rulesByPriority = sorted(rules, key=lambda r: r.getPriority(), reverse=True)
-                for r in rulesByPriority[1:]:
-                    flash(r.buildingName + ", ruleId " + str(r.id) + " ignored.")
+                return rules[0]
 
-                return rulesByPriority[0]
-
-    def conflictResolution(self, ruleList):
+    def conflictResolution(self, ruleList, category):
         flash('Executing CONFLICT RESOLUTION', 'red')
 
-        rulesMap = {r.id: r for r in ruleList}
-        print rulesMap
-
-        intersectionsGroups = self.getIntersections(rulesMap)
-        print intersectionsGroups
-
-        maximumPriority = 0
-        for group in intersectionsGroups:
-            aggregatePriority = 0
-            for g in group:
-                aggregatePriority += rulesMap[g].getPriority()
-
-            print 'agg: ' + str(aggregatePriority)
-
-            if aggregatePriority > maximumPriority:
-                maximumPriority = aggregatePriority
-                selectedGroup = group
-
-        print selectedGroup
-        print maximumPriority
-
-        # If the group is made by more than one rule a new rule has to be synthesized
-        if len(selectedGroup) > 1:
-            selectedRules = {key: rulesMap[key] for key in selectedGroup}
-            return self.springModel(selectedRules)
-        # If just one rule is winner it is returned as is
+        if category in ['HVAC_HUM', 'HVAC_TEMP']:
+            return self.satisfactionResolution(ruleList)
         else:
-            return rulesMap[selectedGroup[0]]
+            return self.majorityWeightedVote(ruleList)
 
-    # Isolate groups of intersections if any and returns a list of groups
-    def getIntersections(self, rulesMap):
+    def satisfactionResolution(self, ruleList):
+        flash('Executing SATISFACTION RESOLUTION', 'red')
+        print 'on rules: ', ruleList
+
         actionManager = ActionManager()
 
-        endPoints = []
-
-        for r in rulesMap.values():
-            action = actionManager.translateAction(r.consequent, getDict=True)
-
-            start = {}
-            start['value'] = action['translatedParams']['0']
-            start['type'] = 'S'
-            start['id'] = r.id
-
-            end = {}
-            end['value'] = action['translatedParams']['1']
-            end['type'] = 'E'
-            end['id'] = r.id
-
-
-            endPoints.append(start)
-            endPoints.append(end)
-
-        # Order in ascending order
-        endPoints = sorted(endPoints, key=lambda x: x['value'], reverse=False)
-
-        print endPoints
-
-        groups = []
-        stack = []
-        for p in endPoints:
-            if p['type'] == 'S':
-                stack.append(p['id'])
-                groups.append(list(stack))
-            else:
-                stack.remove(p['id'])
-                if(len(stack)>0):
-                    groups.append(list(stack))
-
-        return groups
-
-    def springModel(self, rulesGroup):
-        actionManager = ActionManager()
-
-        # Return new Rule instance
-        intersectionStart = -100000
-        intersectionEnd = 100000
-        weightedMeans = 0
-        weightedVariances = 0
-        aggregatePriority = 0
-        for r in rulesGroup.values():
+        for r in ruleList:
+            r = r.getDict()
             action = actionManager.translateAction(r.consequent, getDict=True)
 
             start = float(action['translatedParams']['0'])
             end = float(action['translatedParams']['1'])
 
-            priority = float(r.getPriority())
-
-            # Intersection
-            if start > intersectionStart:
-                intersectionStart = start
-            if end < intersectionEnd:
-                intersectionEnd = end
-
-            # Weighted means and variances
-            weightedMeans += priority * float(start + end)/2
-            weightedVariances += priority * float(end - start)/2
-            aggregatePriority += priority
-
-        # Intersection Mean
-        mu1 = float(intersectionStart + intersectionEnd)/2
-
-        # Weighted Mean
-        mu2 = float(weightedMeans)/aggregatePriority
-
-        # Final mean goal and variance
-        mu = float(mu1 + 5*mu2)/6
-        variance = float(weightedVariances)/aggregatePriority
-
-        print mu1, mu2, mu, variance
+            priority = r['priority']
+            mean = (start + end)/2.0
+            sigma1 = mean - start
+            sigma2 = end - mean
 
 
-        firstRule = rulesGroup.values()[0]
-        commonRule = firstRule.getDict()
-        timestamp = 'NOW'
-        resultingRule = Rule(id='SYNTHETIZED', priority=aggregatePriority, category=commonRule['category'],
+            satisfactionManager = SatisfactionManager()
+
+            values += priority*satisfactionManager.agauss(mean,sigma1,sigma2)
+
+        finalValue = satisfactionManager.getMax(values)
+        finalPriority = satisfactionManager.getMaxPriority(values)
+
+
+        commonRule = ruleList[0].getDict()
+        resultingRule = Rule(id='SYNTHETIZED', priority=finalPriority, category=commonRule['category'],
                              buildingName=commonRule['buildingName'], groupId=commonRule['groupId'],
                              roomName=commonRule['roomName'], authorUuid=commonRule['authorUuid'],
                              antecedent=commonRule['antecedent'], consequent=commonRule['consequent'],
                              enabled=True, deleted=False)
-        print firstRule
-        print firstRule.triggers_returned_values
 
         print resultingRule
-        print resultingRule.triggers_returned_values
+        print finalValue, finalPriority
 
-        # TODO: implement new rule
-        pass
+        return resultingRule
 
 
+    def majorityWeightedVote(self, ruleList):
+        flash('Executing MAJORITY VOTE RESOLUTION', 'red')
+        print 'on rules: ', ruleList
+
+        dict = {}
+
+        for r in ruleList:
+            r = r.getDict()
+            if r['consequent'] not in dict.keys():
+                dict[r['consequent']] = r['priority']
+            else:
+                dict[r['consequent']] += r['priority']
+
+        print dict
+
+        import operator
+        winningConsequent = max(dict.iteritems(), key=operator.itemgetter(1))[0]
+
+
+        commonRule = ruleList[0].getDict()
+        resultingRule = Rule(id='SYNTHETIZED', priority=dict[winningConsequent], category=commonRule['category'],
+                             buildingName=commonRule['buildingName'], groupId=commonRule['groupId'],
+                             roomName=commonRule['roomName'], authorUuid='SYNTHESIZED',
+                             antecedent='SYNTHESIZED', consequent=winningConsequent,
+                             enabled=True, deleted=False)
+
+        print 'winningRule', resultingRule
+
+        return resultingRule
 
     def __str__(self):
         return "ActionExecutor: "
